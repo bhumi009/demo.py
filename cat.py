18b1ee1524643b03c0a988548277a79e692d8965
bhumi009 <145538977+bhumi009@users.noreply.github.com>
2024-01-08 14:16:09 +0530

    Update cat.py

--- cat.py
+++ cat.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config(page_title='Places')
 st.header("Tourism Places")
 
-col1,col2=st.columns(2)
+col1,col2,col3=st.columns(3)
 with col1:
   st.subheader("Simla")
   st.image("./simla.jpg",caption="Simla",width=300,use_column_width=True)
@@ -11,8 +11,8 @@ with col2:
   st.subheader("Dubai")
   st.image("./dubai.jpg",caption="Dubai",width=300,use_column_width=True)
   st.write("dubai is a desert place")
-  
-st.subheader("Doreamon")
-st.video("https://www.youtube.com/watch?v=Ve_Sq9j2VZU&pp=ygUXZG9yZWFtb24gdmlkZW9zIGluIHNub3c%3D",width=300,use_column_width=True)
-st.write("Doraemon is a Japanese manga series written and illustrated by Fujiko F. Fujio.")
+with col3:
+  st.subheader("Doreamon")
+  st.video("https://www.youtube.com/watch?v=Ve_Sq9j2VZU&pp=ygUXZG9yZWFtb24gdmlkZW9zIGluIHNub3c%3D",width=300,use_column_width=True)
+  st.write("Doraemon is a Japanese manga series written and illustrated by Fujiko F. Fujio.")
   
