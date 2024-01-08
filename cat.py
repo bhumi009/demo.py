a300632032c8d0f2cdc2f565e5df66674e5dad8c
bhumi009 <145538977+bhumi009@users.noreply.github.com>
2024-01-08 14:03:27 +0530

    Update cat.py

--- cat.py
+++ cat.py
@@ -11,4 +11,9 @@ with col2:
   st.subheader("Dubai")
   st.image("./dubai.jpg",caption="Dubai",width=300,use_column_width=True)
   st.write("dubai is a desert place")
-
+col1=st.columns(1)  
+with col1:
+  st.subheader("Doreamon")
+  st.video("https://www.youtube.com/watch?v=Ve_Sq9j2VZU&pp=ygUXZG9yZWFtb24gdmlkZW9zIGluIHNub3c%3D",width=300,use_column_width=True)
+  st.write("Doraemon is a Japanese manga series written and illustrated by Fujiko F. Fujio.")
+  
