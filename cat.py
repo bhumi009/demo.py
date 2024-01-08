6f4d321bcbdbb21dac72c4ddafbc3a3f04659155
bhumi009 <145538977+bhumi009@users.noreply.github.com>
2024-01-08 12:17:21 +0530

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,14 +1,14 @@
 import streamlit as st
 st.set_page_config(page_title='cats')
-st.header("Types of cats")
+st.header("Tourism Places")
 
 col1,col2=st.columns(2)
 with col1:
-  st.subheader("persian cat")
-  st.image("./cat1.jpg",caption="persian cat",width=300,use_column_width=True)
-  st.write("Persian cats are cute")
+  st.subheader("Simla")
+  st.image("./simla.jpg",caption="Simla",width=300,use_column_width=True)
+  st.write("simla is cool place")
 with col2:
-  st.subheader("Ragdoll cat")
-  st.image("./cat2.jpg",caption="Regdoll cat",width=300,use_column_width=True)
-  st.write("Regdolls cats are proud")
+  st.subheader("Dubai")
+  st.image("./dubai.jpg",caption="Dubai",width=300,use_column_width=True)
+  st.write("dubai is a desert place")
 
