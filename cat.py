45128ee8a1d90ebbb757e90a9491c148397637e4
bhumi009 <145538977+bhumi009@users.noreply.github.com>
2024-01-08 12:18:03 +0530

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,14 +1,14 @@
 import streamlit as st
-st.set_page_config(page_title='cats')
+st.set_page_config(page_title='Places')
 st.header("Tourism Places")
 
 col1,col2=st.columns(2)
 with col1:
   st.subheader("Simla")
-  st.image("./simla.jpg",caption="Simla",width=300,use_column_width=True)
+  st.image("./simla.jpg",caption="Simla",width=300,use_column_width=True,height=400)
   st.write("simla is cool place")
 with col2:
   st.subheader("Dubai")
-  st.image("./dubai.jpg",caption="Dubai",width=300,use_column_width=True)
+  st.image("./dubai.jpg",caption="Dubai",width=300,use_column_width=True,height=400)
   st.write("dubai is a desert place")
 
