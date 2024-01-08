e3203811681d2c1c8e358b9479f3381a6752e809
bhumi009 <145538977+bhumi009@users.noreply.github.com>
2024-01-08 12:19:05 +0530

    Update cat.py

--- cat.py
+++ cat.py
@@ -5,10 +5,10 @@ st.header("Tourism Places")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("Simla")
-  st.image("./simla.jpg",caption="Simla",width=300,use_column_width=True,height=400)
+  st.image("./simla.jpg",caption="Simla",width=300,use_column_width=True)
   st.write("simla is cool place")
 with col2:
   st.subheader("Dubai")
-  st.image("./dubai.jpg",caption="Dubai",width=300,use_column_width=True,height=400)
+  st.image("./dubai.jpg",caption="Dubai",width=300,use_column_width=True,height=400,,use_column_height=True)
   st.write("dubai is a desert place")
 
